cc61365fc0655101c6655cf6ff80adb1cac16200
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-10-24 14:54:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,17 @@
-// Array de objetos contendo o nome e a nota dos alunos
 const alunos = [
-    { nome: 'João', nota: 7.5 },
-    { nome: 'Ana', nota: 5.8 },
-    { nome: 'Pedro', nota: 6.0 },
-    { nome: 'Mariana', nota: 8.2 },
-    { nome: 'Carlos', nota: 4.3 },
-    { nome: 'Juliana', nota: 9.1 },
-];
+    {nome:'Violeta Pêra', nota: 6},
+    {nome:'Edna Moda', nota:10},
+    {nome:'Helena Pêra', nota: 4},
+    {nome:'Buddy Pine', nota: 3},
+    {nome:'Beto Pera', nota: 2},
+    {nome:' Lúcio Barro', nota: 9},
+    {nome:'Roberto Pêra', nota: 5}
+]
 
-// Função para retornar alunos com nota maior ou igual a 6 utilizando forEach
-function filtraAprovados(alunos) {
-    const aprovados = [];
-    
-    alunos.forEach(function(aluno) {
-        if (aluno.nota >= 6) {
-            aprovados.push(aluno); // Adiciona o aluno ao array de aprovados
-        }
-    });
+const FiltroAlunosAprovado = (alunos) =>
+    alunos.filter(alunos => alunos.nota >= 6);
 
-    return aprovados;
-}
 
-// Chamando a função para obter os alunos aprovados
-const alunosAprovados = filtraAprovados(alunos);
+const alunosAprovados = FiltroAlunosAprovado(alunos)
 
-// Exibindo os alunos aprovados
-console.log(alunosAprovados);
+console.log(alunosAprovados)
